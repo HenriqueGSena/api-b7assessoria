437eb4cf3d9cbfde36b53d41ca5d346746faf1bf
henrique.sena <carloshenrique3250@gmail.com>
2024-12-11 21:43:32 -0300

    Gerando projeto;

---
+++ src/main/java/com/crud/b7assessoria/B7assessoriaApplication.java
@@ -0,0 +1,13 @@
+package com.crud.b7assessoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B7assessoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B7assessoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/b7assessoria/B7assessoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.b7assessoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B7assessoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
