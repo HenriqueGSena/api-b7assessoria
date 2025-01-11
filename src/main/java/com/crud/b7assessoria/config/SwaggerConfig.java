3f53fd5524bc4da59f9307892e8d3cc8b1e4a665
henrique.sena <carloshenrique3250@gmail.com>
2025-01-11 19:59:31 -0300

    Atualizando SwaggerConfig.java

--- src/main/java/com/crud/b7assessoria/config/SwaggerConfig.java
+++ src/main/java/com/crud/b7assessoria/config/SwaggerConfig.java
@@ -33,7 +33,7 @@ public class SwaggerConfig {
                 .version("1.0.0")
                 .description("The goal is to build a Rest API for product management. You should use Java, Spring Boot, and a relational database of your choice. You can use any other libraries/tools that help in the development of the test.")
                 .contact(contact);
-        
+
         SecurityScheme securityScheme = new SecurityScheme()
                 .name("Authorization")
                 .type(SecurityScheme.Type.APIKEY)
